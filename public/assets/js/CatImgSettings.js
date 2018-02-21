e5f773a98c3c49482ae89c465f2250294c86001c
PYEGH <pashayezhov93@gmail.com>
2018-02-22 00:46:57 +0300

    Fixed

---
+++ public/assets/js/CatImgSettings.js
@@ -0,0 +1,36 @@
+class CatImgSettings {
+    constructor() {
+
+    }
+
+    static getRightSettings() {
+        return [
+            {
+                sx: 0,
+                sy: 64,
+                sWidth: 32,
+                sHeight: 32
+            },
+            {
+                sx: 32,
+                sy: 64,
+                sWidth: 32,
+                sHeight: 32
+            },
+            {
+                sx: 64,
+                sy: 64,
+                sWidth: 32,
+                sHeight: 32
+            },
+            {
+                sx: 96,
+                sy: 64,
+                sWidth: 32,
+                sHeight: 32
+            }
+        ];
+    }
+
+}
+export default CatImgSettings;
